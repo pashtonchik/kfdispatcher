f2354011cce8c38760458121619b6c7db346bea0
kkiniuss <kkiniuss03@gmail.com>
2022-12-20 04:48:34 +0300

    fix

--- kfdispatcher.py
+++ kfdispatcher.py
@@ -54,7 +54,7 @@ async def change_status(client, message):
 
 
 
-@app.on_message(filters=filters.user(name_bot) & filters.regex('\w+Заяв\w+'))
+@app.on_message(filters=filters.user(name_bot) & filters.regex('Заявка долго'))
 async def notification(client, message):
     data = {
             "chat_id" : "-1001839190420",
